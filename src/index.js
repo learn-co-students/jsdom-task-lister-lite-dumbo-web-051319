a99e3ebb63661f8f5b2fa70ca9d44a24ac92e1f6
sticklercarla <carlastickler@gmail.com>
2019-06-26 19:07:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -15,22 +15,22 @@ function removeItem(event){
 form.addEventListener("submit", function(event){
     event.preventDefault()
     const taskInfo = document.getElementById("new-task-description")
-console.dir(taskInfo)
-    console.log(typeof taskInfo)
-    if (document.querySelector("value") === "High"){
-      console.log("HI")
-      let priority = taskInfo.style.color = "red"
-    } else if (document.querySelector("value") === "Medium"){
-      let priority = taskInfo.style.color = "yellow"
-    } else {
-      let prioriry = taskInfo.style.color = "green"
-    }
 
 
     const formItem = document.createElement("li")
     formItem.innerText = taskInfo.value
     listItems.append(formItem)
-    formItem.style.color = "green"
+
+    console.log(event.target === form)
+ 
+    if (form.priority.value === "High"){
+      console.log("HI")
+      formItem.style.color = "red"
+    } else if (form.priority.value === "Medium"){
+      formItem.style.color = "yellow"
+    } else {
+      formItem.style.color= "green"
+    }
     formItem.addEventListener("click", removeItem)
 })
 
